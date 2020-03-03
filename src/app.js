f38339122c8eee026611415a8a6d99d56a3def7c
mariamisa <maryaam1998@hotmail.com>
2020-03-03 14:02:03 +0200

    handel routes for error relates #11

--- src/app.js
+++ src/app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 
 const routes = require('./routes');
-const { client, server } = require('./controllers/error');
 
 const app = express();
 
@@ -11,8 +10,5 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(routes);
-app.use(client);
-app.use(server);
-
 
 module.exports = app;
