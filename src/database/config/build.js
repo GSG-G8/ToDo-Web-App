955436acb94629299aafa01f1294138a18391dd6
MohammedAlghazali <hamood-monzer11@hotmail.com>
2020-03-03 15:51:42 +0200

    add build.js relates #22

---
+++ src/database/config/build.js
@@ -0,0 +1,12 @@
+const { readFileSync } = require('fs');
+const { join } = require('path');
+
+const connection = require('./connection');
+
+
+const dbBuild = () => {
+  const sql = readFileSync(join(__dirname, 'db_build.sql')).toString();
+  return connection.query(sql);
+};
+
+module.exports = { dbBuild };
