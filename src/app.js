62df31de5503ca75494361d9ce8b6287c24786c9
MohammedAlghazali <hamood-monzer11@hotmail.com>
2020-03-03 13:55:22 +0200

    handle static file relates #6

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+const { join } = require('path');
 const express = require('express');
 const cookieParser = require('cookie-parser');
 
@@ -9,6 +10,7 @@ app.set('PORT', process.env.PORT || 3030);
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static(join(__dirname, '..', 'public')));
 app.use(routes);
 
 module.exports = app;
