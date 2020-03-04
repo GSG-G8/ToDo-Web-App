815dc07f9d00696193b18ca62e7a7d60c3411546
MohammedAlghazali <hamood-monzer11@hotmail.com>
2020-03-04 13:54:41 +0200

    handle login route relates #4

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,5 +1,8 @@
 const validateSignUpData = require('./validateUserSignUp');
 const checkNewUserExist = require('./checkNewUserExist');
 const signUserUp = require('./signUserUp');
+const checkEmail = require('./checkExist ');
 
-module.exports = { validateSignUpData, checkNewUserExist, signUserUp };
+module.exports = {
+  validateSignUpData, checkNewUserExist, signUserUp, checkEmail,
+};
