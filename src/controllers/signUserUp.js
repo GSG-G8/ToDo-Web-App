f244ddefd2108076d91d9299d47c2999ecaca877
AhmedSafi97 <ahmadhsafi1997@gmail.com>
2020-03-04 11:11:49 +0200

    removed console.log relates #19

--- src/controllers/signUserUp.js
+++ src/controllers/signUserUp.js
@@ -2,14 +2,12 @@ const bcrypt = require('bcryptjs');
 const rejesterNewUserQuery = require('../database/queries/signup');
 
 module.exports = (req, res, next) => {
-  console.log(req.body.psw);
   bcrypt.hash(req.body.psw, 10).then((hashedPassword) => {
     const newUser = {
       name: `${req.body.fname} ${req.body.lname}`,
       email: req.body.email,
       password: hashedPassword,
     };
-    console.log(hashedPassword);
-    rejesterNewUserQuery(newUser).then(() => { console.log('here'); res.json({ msg: 'signed up successfuly' }); });
+    rejesterNewUserQuery(newUser).then(() => res.json({ msg: 'signed up successfuly' }));
   }).catch(next);
 };
--- src/database/queries/checkUserEmail.js
+++ src/database/queries/checkUserEmail.js
@@ -2,7 +2,7 @@ const connection = require('../config/connection');
 
 module.exports = (email) => {
   const sql = {
-    text: 'select password from users where email = $1',
+    text: 'select * from users where email = $1',
     values: [email],
   };
   return connection.query(sql);
