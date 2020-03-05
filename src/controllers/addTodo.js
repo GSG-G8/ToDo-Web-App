ae54e34328155cf7fbe3d2e5285a29086e7cf232
MohammedAlghazali <hamood-monzer11@hotmail.com>
2020-03-05 10:08:05 +0200

    fix problem

--- src/controllers/addTodo.js
+++ src/controllers/addTodo.js
@@ -1,8 +1,10 @@
+const { join } = require('path');
+
 const addtodo = require('../database/queries/addtodo');
 
 module.exports = (req, res) => {
   const { description } = req.body;
   const userId = req.id;
   addtodo(userId, description);
-  res.redirect('/');
+  res.sendFile(join(__dirname, '..', '..', 'public', 'main.html'));
 };
